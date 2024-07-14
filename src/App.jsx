0af0084e5c0def750bbd3be4494eaa5567870b87
abdulrahim-ramadan <abdalrhim19991@gmail.com>
2024-07-14 18:15:59 +0200

    add events

--- src/App.jsx
+++ src/App.jsx
@@ -5,12 +5,29 @@ import CreatePost from './components/CreatePost'
 
 function App() {
 
-  const author = "Abdulrahim Ramada"
+  const authorName = "Yusof "
+
+  const hello = () =>{
+    console.log('Welcome')
+  }
+
+  const hello2 = (name) =>{
+    console.log('Hello ${name}')
+  }
+
+  const hello3 = (e) =>{
+    console.log(e)
+  }
 
   return (
     <>
+      <button onClick={hello}> Click </button>
+      <button onClick={hello3}> Click again </button>
+      <button onClick={()=> hello2('Yusof')}> Click again </button>
+
+
       < CreatePost />
-      < Posts author={author} />
+      < Posts author={authorName} />
     </>
   )
 }
